074a2db4952b4d278e42a58a57c4b7114b164e8c
Simone Roveri <spikeroveri91@gmail.com>
2024-01-25 12:10:25 +0900

    parsing csv file

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,8 +5,15 @@ class FilesController < ApplicationController
   end
 
   def import
-    return redirect_to root_path, notice: "Select CSV only CSV file please" unless params[:file].content_type == "text/csv"
-    # binding.b ＃use for  debugging can visualise what happen inside the request
+    file = params[:file]
+    return redirect_to root_path, notice: "Select CSV only CSV file please" unless file.content_type == "text/csv"
+    # binding.b   use for  debugging can visualise what happen inside the request
+
+    file = File.open(file)
+    csv = CSV.parse(file, headers: true)
+
+    binding.b
+
     redirect_to root_path, notice: "file successufully imported!"
   end
 end
