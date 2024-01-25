ec34e516bf4e0503bb1f3668e63bc6c02149c3de
Simone Roveri <spikeroveri91@gmail.com>
2024-01-25 11:25:14 +0900

    add a check to the front end so that only CSV file can be updated

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,12 @@
 class FilesController < ApplicationController
+  require "csv"
+
   def home
   end
 
   def import
     return redirect_to root_path, notice: "Select CSV only CSV file please" unless params[:file].content_type == "text/csv"
-    # binding.b
+    # binding.b ＃use for  debugging can visualise what happen inside the request
     redirect_to root_path, notice: "file successufully imported!"
   end
 end
