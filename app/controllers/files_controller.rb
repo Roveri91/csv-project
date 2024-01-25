ce616bcfe4ea9950ac8fe4e8f51f9ab34a15d0cc
Simone Roveri <spikeroveri91@gmail.com>
2024-01-25 09:56:24 +0900

    added check for import only CSV file in the Backend

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,6 +3,8 @@ class FilesController < ApplicationController
   end
 
   def import
-    binding.b
+    return redirect_to root_path, notice: "Select CSV only CSV file please" unless params[:file].content_type == "text/csv"
+    # binding.b
+    redirect_to root_path, notice: "file successufully imported!"
   end
 end
