3bcbe36bb0907531829708f53d13a47e180b5c37
Simone Roveri <spikeroveri91@gmail.com>
2024-01-26 17:07:56 +0900

    add back button to form and fic

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  root "files#home"
   # post "/import", to: "files#import"
   # get "files/new", to: "files#new"
   # post "/file", to: "files#create"
 
   resources :csv_files, only: [:index, :new, :create]
+  root "csv_files#index"
 
 
   # Defines the root path route ("/")
