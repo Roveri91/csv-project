eacbc6f634fe7d78c2eb57381a384b5072d96611
Simone Roveri <spikeroveri91@gmail.com>
2024-01-26 16:53:40 +0900

    delete files controller

--- app/controllers/csv_files_controller.rb
+++ app/controllers/csv_files_controller.rb
@@ -1,4 +1,6 @@
 class CsvFilesController < ApplicationController
+  require "csv"
+
   def index
     @csv_files= CsvFile.all
   end
@@ -9,7 +11,31 @@ class CsvFilesController < ApplicationController
 
   def create
     @csv_file = CsvFile.new(csv_file_parmas)
-    @csv_file.save
+    if @csv_file.save
+      redirect_to csv_files_path, notice: "file saved on active storage"
+    end
+  end
+
+  def import
+    file = params[:file]
+    return redirect_to root_path, notice: "Select CSV only CSV file please" unless file.content_type == "text/csv"
+    # binding.b   use for  debugging can visualise what happen inside the request
+
+    file = File.open(file)
+    csv = CSV.parse(file, headers: true)
+    #col_sep: ';' if the file is separeted by semicol instead of comma
+    @user_hash = {}
+
+    csv.each do |row|
+      @user_hash[:name] = row["Name"]
+      @user_hash[:age] = row["Age"]
+      @user_hash[:city] = row["City"]
+
+      # create model passing the hash
+      # User.create(@user_hash)
+    end
+
+    redirect_to root_path, notice: "file successufully imported!"
   end
 
   private
--- test/controllers/files_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class FilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get files_home_url
-    assert_response :success
-  end
-end
