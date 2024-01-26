fe9d1924a5244d351fdb6940625cdb5d9a6cf49f
Simone Roveri <spikeroveri91@gmail.com>
2024-01-26 16:33:07 +0900

    successufully saved file as attachment

--- app/controllers/csv_files_controller.rb
+++ app/controllers/csv_files_controller.rb
@@ -1,7 +1,22 @@
 class CsvFilesController < ApplicationController
   def index
+    @csv_files= CsvFile.all
   end
 
   def new
+    @csv_file = CsvFile.new
   end
+
+  def create
+    @csv_file = CsvFile.new(csv_file_parmas)
+    @csv_file.save
+  end
+
+  private
+
+  def csv_file_parmas
+    params.require(:csv_file).permit(:file)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'csv_files/index'
-  get 'csv_files/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
+
   root "files#home"
-  post "/import", to: "files#import"
-  get "files/new", to: "files#new"
-  post "/file", to: "files#create"
+  # post "/import", to: "files#import"
+  # get "files/new", to: "files#new"
+  # post "/file", to: "files#create"
+
+  resources :csv_files, only: [:index, :new, :create]
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
