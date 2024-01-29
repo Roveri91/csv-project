ffebc45bec711125c10d9261831796b74e1a059d
Simone Roveri <spikeroveri91@gmail.com>
2024-01-29 13:53:49 +0900

    create user model

--- app/sidekiq/hard_job.rb
+++ app/sidekiq/hard_job.rb
@@ -1,7 +1,25 @@
 class HardJob
   include Sidekiq::Job
 
+  require 'csv'
+
   def perform(*args)
     # Do something
   end
+
+  def perform_async(file)
+    file = File.open(file)
+    csv = CSV.parse(file, headers: true)
+    @user_hash = {}
+
+    csv.each do |row|
+      @user_hash[:name] = row["Name"]
+      @user_hash[:age] = row["Age"]
+      @user_hash[:city] = row["City"]
+
+      User.create(@user_hash)
+    end
+  end
+
+  # HardJob.perform_async('bob', 5)
 end
