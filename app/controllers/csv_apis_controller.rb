ead2ea5b571fc651ed1c863543d269e36b483135
Simone Roveri <spikeroveri91@gmail.com>
2024-02-08 16:04:34 +0900

    create new sidekiq job for parsing API

--- app/controllers/csv_apis_controller.rb
+++ app/controllers/csv_apis_controller.rb
@@ -10,12 +10,12 @@ class CsvApisController < ApplicationController
 
     url  = "http://localhost:4000/users"
 
-    file_serialized = URI.open(url).read
-    users = JSON.parse(file_serialized)
+    # file_serialized = URI.open(url).read
+    # users = JSON.parse(file_serialized)
 
-    users.each do |user|
-      User.create(user)
-    end
+    # users.each do |user|
+    #   User.create(user)
+    # end
 
     redirect_to :root, notice: "Successfully Imported from #{url}"
   end
