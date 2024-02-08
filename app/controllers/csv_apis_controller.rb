68ce8fff3d9d93859cb67e1972424e64140d8832
Simone Roveri <spikeroveri91@gmail.com>
2024-02-08 15:42:19 +0900

    parsing data from API

--- app/controllers/csv_apis_controller.rb
+++ app/controllers/csv_apis_controller.rb
@@ -1,8 +1,23 @@
 class CsvApisController < ApplicationController
+  require 'json'
+  require 'open-uri'
+
   def index
   end
 
   def api_download
-    redirect_to :root, notice: "YEAH!!"
+
+    url  = "http://localhost:4000/users"
+
+    file_serialized = URI.open(url).read
+    user = JSON.parse(file_serialized)
+
+    # CSV.read(file_serialized)
+
+    # CSV.foreach(file_serialized, headers: :first_row) do |row|
+    #   puts "#{row['name']}"
+    # end
+
+    redirect_to :root, notice: user
   end
 end
